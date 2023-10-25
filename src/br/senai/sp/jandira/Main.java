8e5ac6f9575cfb50ed1866d6a425d32b16369d14
Vitoria <vazevedo73@gmail.com>
2023-10-25 17:13:17 -0300

    finalizado

--- src/br/senai/sp/jandira/Main.java
+++ src/br/senai/sp/jandira/Main.java
@@ -8,11 +8,13 @@ public class Main {
         Gerente gerente1 = new Gerente("João", 12);
         gerente1.setBonus(200);
         gerente1.calcularSalario();
-        System.out.println(gerente1.getSalarioBase());
+        System.out.println("Nome: " + gerente1.getNome());
+        System.out.println("Salário: " + gerente1.getSalarioBase());
 
         Programador programador1 = new Programador("Marta", 13);
         programador1.setHoraExtra(15);
         programador1.calcularSalario();
-        System.out.println(programador1.getSalarioBase());
+        System.out.println("Nome: " + programador1.getNome());
+        System.out.println("Salário: " + programador1.getSalarioBase());
     }
 }
